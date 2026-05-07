e4820df50a160abd4f7fe01cbe153dee1a1b6f4a
Andrew <elch.16@yandex.ru>
2026-05-07 15:18:11 +0300

    fix dotenv

--- server/src/main/kotlin/infrastructure/db/DatabaseFactory.kt
+++ server/src/main/kotlin/infrastructure/db/DatabaseFactory.kt
@@ -11,14 +11,16 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init() {
-        val dotenv = dotenv{ignoreIfMissing = true}
+        val dotenv = dotenv { ignoreIfMissing = true }
         val driverClassName = "org.postgresql.Driver"
-        val jdbcUrl = System.getenv("POSTGRES_URL") ?: "jdbc:postgresql://localhost:5432/todolist_db"
+        val jdbcUrl =
+            System.getenv("POSTGRES_URL") ?: "jdbc:postgresql://localhost:5432/todolist_db"
 
         val user = System.getenv("POSTGRES_USER") ?: dotenv["DB_USER"] ?: "postgres"
         val password = System.getenv("POSTGRES_PASSWORD") ?: dotenv["DB_PASSWORD"] ?: "postgres"
 
-        val database = Database.connect(createHikariDataSource(jdbcUrl, driverClassName, user, password))
+        val database =
+            Database.connect(createHikariDataSource(jdbcUrl, driverClassName, user, password))
 
         transaction(database) {
             SchemaUtils.create(UsersTable, TasksTable)
